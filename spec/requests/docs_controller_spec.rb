ef1f68f518dd8fb9b0b50d2f71ca2849c128503f
nickchomey <88559987+nickchomey@users.noreply.github.com>
2021-11-24 13:59:47 -0600

    comment out date sort

--- spec/requests/docs_controller_spec.rb
+++ spec/requests/docs_controller_spec.rb
@@ -163,7 +163,7 @@ describe Docs::DocsController do
         end
       end
 
-      context 'by date' do
+     =begin context 'by date' do
         before do
           topic2.update(last_posted_at: Time.zone.now + 100)
         end
@@ -191,7 +191,7 @@ describe Docs::DocsController do
           expect(topics[0]['id']).to eq(topic2.id)
           expect(topics[1]['id']).to eq(topic.id)
         end
-      end
+      end =end
     end
 
     context 'when searching' do
